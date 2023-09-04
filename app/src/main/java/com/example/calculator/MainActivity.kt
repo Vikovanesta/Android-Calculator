edd2969df29fad516df62a3213f2c3b8dd758bf0
Vikovanesta <Vikovanesta@gmail.com>
2023-09-04 21:47:56 +0700

    Implement logic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,15 +5,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.calculator.ui.theme.CalculatorTheme
+import java.util.Stack
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,30 +13,107 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    var valueList = intArrayOf()
-    var operationList = charArrayOf()
-
     fun InputNilai(view: View) {
+        val panel = findViewById<TextView>(R.id.panel)
         when((view as Button).text.toString()){
-            "C"-> {
-                findViewById<TextView>(R.id.panel).text = "Halo"
+            "AC"-> {
+                panel.text = ""
             }
-            "-"-> {
-//                todo: tambahkan fungsi pengurangan
+            "DEL"-> {
+                if (panel.text.last() == ' ') {
+//                    panel.text = panel.text.dropLast(3)
+                    panel.text = panel.text.dropLastWhile { !it.isDigit() }
+                } else {
+                    panel.text = panel.text.dropLast(1)
+                }
             }
-            "x"-> {
-//                 todo: tambahkan fungsi perkalian
-            }
-            "/"-> {
-//                todo: tambahkan fungsi pembagian
+            "+", "-", "/", "x"-> {
+                if (panel.text.last() == ' ') {
+                    panel.text = panel.text.dropLastWhile { !it.isDigit() }
+                    panel.text = "${panel.text} ${view.text} "
+                } else {
+                    panel.text = "${panel.text} ${view.text} "
+                }
             }
             else->{
-                findViewById<TextView>(R.id.panel).text =
-                    findViewById<TextView>(R.id.panel).text.toString()+(view as Button).text.toString()
+                panel.text = "${panel.text}${view.text}"
             }
         }
     }
 
-    fun calculate(view: View) {}
+    fun calculate(view: View) {
+        val panel = findViewById<TextView>(R.id.panel)
+        val expression = getExpression(panel.text.toString())
+        val stack = Stack<Int>()
+
+        for (token in expression) {
+            if (token !in "+-*/") {
+                stack.push(token.toInt())
+            } else {
+                val operand2 = stack.pop()
+                val operand1 = stack.pop()
+                when (token) {
+                    "+" -> stack.push(operand1 + operand2)
+                    "-" -> stack.push(operand1 - operand2)
+                    "*" -> stack.push(operand1 * operand2)
+                    "/" -> stack.push(operand1 / operand2)
+                }
+            }
+        }
+
+
+//        panel.text = operand1.toString()
+        panel.text = stack.pop().toString()
+//        panel.text = expression.toString()
+    }
+
+    private fun getExpression(expression: String): List<String> {
+        var expression = expression
+        expression = expression.replace(Regex("x"), "*")
+        return infixToPostfix(expression)
+    }
+
+    private fun infixToPostfix(infixExpression: String): List<String> {
+        val operatorPrecedence = mapOf(
+            "+" to 1,
+            "-" to 1,
+            "*" to 2,
+            "/" to 2,
+            "^" to 3
+        )
+        val postfixExpression = mutableListOf<String>()
+        val stack = Stack<String>()
+        val infixExpression = infixExpression.split(" ")
+
+        for (token in infixExpression) {
+            when {
+                token.contains("[0-9]".toRegex()) -> postfixExpression.add(token)
+                token == "(" -> stack.push(token)
+                token == ")" -> {
+                    while (!stack.isEmpty() && stack.peek() != "(") {
+                        postfixExpression.add(stack.pop())
+                    }
+                    stack.pop() // Pop the '('
+                }
+                else -> {
+                    while (!stack.isEmpty() && stack.peek() != "(" &&
+                        operatorPrecedence.getValue(token) <= operatorPrecedence.getOrDefault(stack.peek(), 0)
+                    ) {
+                        postfixExpression.add(stack.pop())
+                    }
+                    stack.push(token)
+                }
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            if (stack.peek() == "(") {
+                throw IllegalArgumentException("Mismatched parentheses in the infix expression")
+            }
+            postfixExpression.add(stack.pop())
+        }
+
+        return postfixExpression
+    }
 }
 
